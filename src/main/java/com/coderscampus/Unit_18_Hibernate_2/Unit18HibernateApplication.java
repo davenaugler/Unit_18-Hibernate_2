10de0189ddb58f92874d374cd0a98c56c30dd335
Dave Naugler <davenaugler@gmail.com>
2024-05-27 12:24:51 -0600

    Starter Java/Spring/Hibernate project

---
+++ src/main/java/com/coderscampus/Unit_18_Hibernate_2/Unit18HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit_18_Hibernate_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Unit18HibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Unit18HibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/Unit_18_Hibernate_2/Unit18HibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus.Unit_18_Hibernate_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit18HibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
