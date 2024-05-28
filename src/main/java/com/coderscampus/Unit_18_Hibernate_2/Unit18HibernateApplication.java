acca91346f20090fa7dfc372cedf75f7cffa5f4e
Dave Naugler <davenaugler@gmail.com>
2024-05-28 16:23:05 -0600

    Configuring DB in properties file
    Added .env file to safely handle DB username and password

--- src/main/java/com/coderscampus/Unit_18_Hibernate_2/Unit18HibernateApplication.java
+++ src/main/java/com/coderscampus/Unit_18_Hibernate_2/Unit18HibernateApplication.java
@@ -1,5 +1,6 @@
 package com.coderscampus.Unit_18_Hibernate_2;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Unit18HibernateApplication {
 
 	public static void main(String[] args) {
+
+//		Content added
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("MYSQL_USERNAME", dotenv.get("MYSQL_USERNAME"));
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+//		End of content added
 		SpringApplication.run(Unit18HibernateApplication.class, args);
 	}
-
 }
+
+
