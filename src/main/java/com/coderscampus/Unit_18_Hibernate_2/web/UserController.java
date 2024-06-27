2ac092b4372812ca6b0b3e4fbc8032ea6053c049
Dave Naugler <davenaugler@gmail.com>
2024-06-27 13:43:26 -0600

    Mapped One to Many relationship for Account to Transaction w/ content

--- src/main/java/com/coderscampus/Unit_18_Hibernate_2/web/UserController.java
+++ src/main/java/com/coderscampus/Unit_18_Hibernate_2/web/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.List;
 
 @Controller
 public class UserController {
